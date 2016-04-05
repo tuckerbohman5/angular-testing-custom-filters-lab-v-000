6271715c26d01a63f4dafe3fae3ad129f488f465
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-05 16:34:26 -0600

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,26 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+  it('should return only persons with given favorite food, ice cream', function(){
+    var mockUsers = [
+    {
+      name: "John Doe",
+      favoriteFood: "ice cream"
+    },
+    {
+      name: "Steve Jobs",
+      favoriteFood: "apple"
+    },
+    {
+      name: "Peter Piper",
+      favoriteFood: "ice cream"
+    }
+    ]
+
+
+    var results = $filter('favoriteFood')(mockUsers, "ice cream");
+    expect(results.length).toEqual(2);
+    expect(results[1].name).toEqual("Peter Piper");
+  });
 	
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -7,4 +7,10 @@ describe('removeAllVowels Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+  it('should remove all vowels in a given string', function(){
+    var testString = "I am learning how to test javascript filters";
+    var results = $filter('removeAllVowels')(testString);
+    expect(results).toEqual(" m lrnng hw t tst jvscrpt fltrs");
+  });
+
 });
